5345cc25a0c250195548e137be8ffedc7ac4bff0
Chris Blust <chblust98@gmail.com>
2018-01-17 20:04:52 -0500

    Delete killHardware.sh

--- launchScripts/killHardware.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#while IFS='' read -r line || [[ -n "$line" ]]; do
-#    kill -s SIGINT "$(($line))"
-#done < /tmp/hardware.pid
-
-`/sbin/ip link set can0 down`
-
-rm /tmp/hardware.pid
