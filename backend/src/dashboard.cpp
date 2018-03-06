3140b54344e2329bf90f4e89a1add5243d933c04
ritfsaecdaq <chblust98@gmail.com>
2018-03-05 22:13:52 -0500

    defined general behavior of dash

--- backend/src/dashboard.cpp
+++ backend/src/dashboard.cpp
@@ -1,26 +1,45 @@
 #include "dashboard.h"
+#include "dash_model.h" // TODO
+#include "shift_controller.h" // TODO
+#include "informer.h" // TODO
 #include <iostream>
 #include <csignal>
 #include <string>
 #include <unistd.h>
 #include <stdio.h>
+
 using namespace std;
 
-static bool go = true;
-
+static informer inf; // the object that collects data and sends to dash
 void sigint_handle(int signal){
-    go = false;
+    inf.finish();
+}
+
+/**
+ * Define shift GPIO interrupts
+ * Start autoup listen thread
+ * Create model, create informer and start it
+ **/
+void initialize(op_mode mode, string filename){
+	shift_controller shiftc;
+	shiftc.begin(); // spawns shift thread
+	dash_model model; // create model
+	
+	inf = informer.get_informer(mode);
+	inf.connect(model);
+	inf.begin(); // loops reading and sending info
+			
 }
 
 int main(int argc, char** argv){
 	// set up the signal handler
    	signal(SIGINT, sigint_handle);
 	
-   	//get command line arguments
-	op_mode mode;	
+   	// get command line arguments
+	op_mode mode; // mode of operation
+	string filename; // name of file (if testdata)
 	int c;
 	while((c = getopt(argc, argv, "m:")) != -1){
-		printf("%d",c);
 		switch(c){
 		case 'm':
 			{
@@ -44,5 +63,7 @@ int main(int argc, char** argv){
 			return EXIT_FAILURE;	
 		}
 	}
+
+	initialize(mode, filename);
     	return EXIT_SUCCESS;
 }
